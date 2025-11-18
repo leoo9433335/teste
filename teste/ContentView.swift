f68fef25f76073c40183a5235eb1446f6f1febbf
Leonardo duarte <silvaleonardoduarte2222@gmail.com>
2025-11-18 08:38:48 -0300

    Initial Commit

---
+++ teste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teste
+//
+//  Created by aluno-22 on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.color)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by aluno-22 on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
