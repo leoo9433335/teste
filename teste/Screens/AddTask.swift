9fe663610f24a855eb09b037c2f6eb9f461b3785
Leonardo duarte <silvaleonardoduarte2222@gmail.com>
2025-11-21 10:47:15 -0300

    add task  text and text field

--- teste/Screens/AddTask.swift
+++ teste/Screens/AddTask.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct AddTask: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State var name: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    
+                    // todo: input name
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Task")
+                            .font(.system(.subheadline, weight: .semibold))
+                            .padding(.horizontal)
+                        TextField("Your task here", text: $name, axis: .vertical)
+                            .padding(.horizontal)
+                            .padding(.vertical, 12)
+                            .background(
+                                RoundedRectangle(cornerRadius: 26)
+                                    .foregroundStyle(.backgroundTertiary)
+                            )
+                    }
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Description")
+                            .font(.system(.subheadline, weight: .semibold))
+                            .padding(.horizontal)
+                        TextField("More details about the task", text: $name, axis: .vertical)
+                            .lineLimit(5...10)
+                            .padding(.horizontal)
+                            .padding(.vertical, 12)
+                            .background(
+                                RoundedRectangle(cornerRadius: 26)
+                                    .foregroundStyle(.backgroundTertiary)
+                            )
+                    }
+                }
+                .padding(.top)
+                .padding(.horizontal)
+            }
+            .background(.backgroundSecondary)
+            .navigationTitle(Text("Add Task"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", systemImage: "xmark") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add", systemImage: "paperplane") {
+                        // Add action here
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+        }
     }
 }
 
